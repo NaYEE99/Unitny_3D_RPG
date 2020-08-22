8d254c127820844865bfaa378fe34c0b184b8e77
NaYEE99 <66513014+NaYEE99@users.noreply.github.com>
2020-08-22 22:14:22 +0800

    2020/08/22 第二版
    進度完成至 20-08 限制移動座標
    
    進度全部追趕完成，但隱藏滑鼠功能沒有弄，要的話之後再補上。

--- Unity_3D_RPG/Assets/Script/Player.cs
+++ Unity_3D_RPG/Assets/Script/Player.cs
@@ -9,6 +9,8 @@ public class Player : MonoBehaviour
 
     [Header("速度"), Range(0, 100)]
     public float speed = 1;
+    [Header("旋轉速度"), Range(0, 100)]
+    public float turn = 1;
 
     private float attack = 10;
     private float hp = 100;
@@ -19,11 +21,14 @@ public class Player : MonoBehaviour
     private Rigidbody rig;
     private Animator ani;
 
+    private Transform cam;      // 攝影機根物件
+
     // 欄位區域結束
     #endregion
 
 
     //==========我是分隔線ˊˇˋ==========//
+
 
     #region 事件區域
 
@@ -32,20 +37,23 @@ public class Player : MonoBehaviour
         rig = GetComponent<Rigidbody>();        // 使用API：GetComponent<泛型：任何物件類型>，抓取此物件上的同類型物件
 
         ani = GetComponent<Animator>();         // 抓取 Animator 給此C#使用
+
+        cam = GameObject.Find("攝影機根物件").transform;  // 在場景直接搜尋("")中的物件
     }
 
     // FixUpdate 為官方建議使用，會延遲 Update 一個影格的時間持續執行
     private void FixedUpdate()
     {
-        Move();                 // 呼叫移動
+        Move();     // 呼叫移動
     }
 
-
+    // 事件區域結束
     #endregion
 
 
-
     //==========我是分隔線ˊˇˋ==========//
+
+
     #region 方法區域
 
     /// <summary>
@@ -55,10 +63,21 @@ public class Player : MonoBehaviour
     {
         float v = Input.GetAxis("Vertical");                            // Vertical為讀取玩家的↑↓ＷＳ的鍵位  
         float h = Input.GetAxis("Horizontal");                          // Horizontal為讀取玩家的→←ＡＤ的鍵位  
-        Vector3 pos = transform.forward * v + transform.right * h;      // 移動座標pos = 物件座標.前方 * v + 物件座標.右方 * h
+        Vector3 pos = cam.forward * v + cam.right * h;                  // 移動座標pos = 攝影機.前方 * v + 攝影機.右方 * h
         rig.MovePosition(transform.position + pos * speed / 10);        // 移動座標(原本座標 + pos 乘上 速度)
 
         ani.SetFloat("移動", Mathf.Abs(v) + Mathf.Abs(h));              // 設定ani的浮點數為("移動", 絕對值v, 絕對值h)
+
+
+        if (v != 0 || h !=0)
+        {
+            pos.y = 0;      // 設定座標 y 固定不動，以防人物模型傾斜
+            Quaternion angle = Quaternion.LookRotation(pos);                            // B 角度angle = 面向角度
+            transform.rotation = Quaternion.Slerp(transform.rotation, angle, turn);     // A 角度 = 角度.插值(A角度, B角度, 旋轉速度)
+        }
+
+
+
     }
 
 
