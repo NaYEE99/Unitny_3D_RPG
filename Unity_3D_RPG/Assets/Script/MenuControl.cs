483cc5ff0b49d0bd74c153640ccd9ef49b8b9c8d
NaYEE99 <66513014+NaYEE99@users.noreply.github.com>
2020-08-22 19:42:02 +0800

    2020/08/22 第一版
    進度補足到 19-11 移動動畫
    
    進度跟上嚕，凱爾除了動畫以外移動功能已完成。

--- Unity_3D_RPG/Assets/Script/MenuControl.cs
+++ Unity_3D_RPG/Assets/Script/MenuControl.cs
@@ -15,7 +15,8 @@ public class MenuControl : MonoBehaviour
     public Image imgLoading;
     [Header("載入場景的名稱")]
     public string nameScene = "遊戲場景";
-
+    [Header("提示")]
+    public GameObject tip;
 
     /// <summary>
     /// 離開遊戲：關閉遊戲
@@ -41,14 +42,26 @@ public class MenuControl : MonoBehaviour
         AsyncOperation ao = SceneManager.LoadSceneAsync(nameScene);     // 異步載入場景(場景名稱)
         ao.allowSceneActivation = false;                                // 取消自動載入
 
-        // 當場景尚未載入完成時 執行
+        // 當場景尚未載入完成時 執行    (!為反向輸入，可當反向閘使用)
         while (!ao.isDone)
-        {
-            textLoading.text = ao.progress * 100 + "%";     // 更新文字
-            imgLoading.fillAmount = ao.progress;            // 更新吧條
+        {   
+            // progress 值域範圍為 0-0.9f，故需要 除以 0.9f 以修正值域至0-1。
+            // Tostring("F+任意整數") 即可控制顯示至小數點後幾位，ex:("f0")時，顯示至100.；("f2")時，顯示至100.00。
+            textLoading.text = (ao.progress / 0.9F * 100).ToString("F2") + "%";     // 更新文字
+            imgLoading.fillAmount = ao.progress / 0.9F;                             // 更新吧條
+            yield return null;                                                      // null為一個影格的時間
 
-            yield return null;                              // null為一個影格的時間
+
+            // 當場景載入完成時 執行(ao.progress = 0.9f 即載入進度完成)
+            if (ao.progress == 0.9f)
+            {
+                tip.SetActive(true);                                            // 顯示(呼叫)提示文字
+
+                if (Input.anyKeyDown == true) ao.allowSceneActivation = true;       // 如果 按下任意鍵 則允許自動載入
+            }
+
         }
+
     }
 
 
---
+++ Unity_3D_RPG/Assets/Script/Player.cs
@@ -0,0 +1,107 @@
+﻿
+
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    #region 欄位區域
+
+    [Header("速度"), Range(0, 100)]
+    public float speed = 1;
+
+    private float attack = 10;
+    private float hp = 100;
+    private float mp = 100;
+    private float exp;
+    private int lv = 1;
+
+    private Rigidbody rig;
+    private Animator ani;
+
+    // 欄位區域結束
+    #endregion
+
+
+    //==========我是分隔線ˊˇˋ==========//
+
+    #region 事件區域
+
+    private void Awake()    // Awake：啟動序列較優先，會在程式開始後立刻執行
+    {
+        rig = GetComponent<Rigidbody>();        // 使用API：GetComponent<泛型：任何物件類型>，抓取此物件上的同類型物件
+
+        ani = GetComponent<Animator>();         // 抓取 Animator 給此C#使用
+    }
+
+    // FixUpdate 為官方建議使用，會延遲 Update 一個影格的時間持續執行
+    private void FixedUpdate()
+    {
+        Move();                 // 呼叫移動
+    }
+
+
+    #endregion
+
+
+
+    //==========我是分隔線ˊˇˋ==========//
+    #region 方法區域
+
+    /// <summary>
+    /// 移動方法：使用 Input.GetAxis("Vertical"、"Horizontal")來達成鍵位偵測
+    /// </summary>
+    private void Move()
+    {
+        float v = Input.GetAxis("Vertical");                            // Vertical為讀取玩家的↑↓ＷＳ的鍵位  
+        float h = Input.GetAxis("Horizontal");                          // Horizontal為讀取玩家的→←ＡＤ的鍵位  
+        Vector3 pos = transform.forward * v + transform.right * h;      // 移動座標pos = 物件座標.前方 * v + 物件座標.右方 * h
+        rig.MovePosition(transform.position + pos * speed / 10);        // 移動座標(原本座標 + pos 乘上 速度)
+
+        ani.SetFloat("移動", Mathf.Abs(v) + Mathf.Abs(h));              // 設定ani的浮點數為("移動", 絕對值v, 絕對值h)
+    }
+
+
+    private void Attack()
+    {
+
+    }
+
+
+    private void Skill()
+    {
+
+    }
+
+
+    private void Hit()
+    {
+
+    }
+
+
+    private void Dead()
+    {
+
+    }
+
+
+    private void GetProp()
+    {
+
+    }
+
+
+    private void Exp()
+    {
+
+    }
+
+
+    // 方法區域結束
+    #endregion
+
+
+
+
+}
